d89977c6b53eb3968bc39306bae65b14a2748923
JacobAguirre9 <jaguirre31@gatech.edu>
2022-04-17 02:15:34 -0400

    update

---
+++ logistic_regression_MNIST.py
@@ -0,0 +1,4 @@
+# This is code for the creation of an algorithm for Isomap and Local linear embedding
+# This code was developed for MATH 4803 taught by Dr. Liao at Georgia Institute of Technology
+
+print("Hello World")
