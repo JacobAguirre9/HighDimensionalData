d29447af1691d32fd41ea46c616118bfc4f5a405
JacobAguirre9 <jaguirre31@gatech.edu>
2022-04-17 02:18:40 -0400

    update

--- logistic_regression_MNIST.py
+++ logistic_regression_MNIST.py
@@ -2,3 +2,9 @@
 # This code was developed for MATH 4803 taught by Dr. Liao at Georgia Institute of Technology
 
 print("Hello World")
+
+from sklearn.datasets import load_digits
+digits = load_digits()
+type(digits.data)
+
+(digits.data.shape, digits.target.shape, digits.images.shape)
